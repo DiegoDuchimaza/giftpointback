324c6d85849b96bd0cbbf6d961212f0d17e44cee
diego duchimaza <duchimazadiego68@gmail.com>
2025-07-02 12:31:21 -0500

    giftcardcontroller creado

---
+++ src/controllers/giftcardController.js
@@ -0,0 +1,50 @@
+const productModel = require('../models/giftcardModel');
+
+const createProduct = (req, res) => {
+  const product = req.body;
+  productModel.create(product, (err) => {
+    if (err) return res.status(500).json({ error: 'Error al crear una tarjeta de regalo' });
+    res.status(201).json({ message: 'Tarjeta de regalo creada' });
+  });
+};
+
+const getProducts = (req, res) => {
+  productModel.findAll((err, products) => {
+    if (err) return res.status(500).json({ error: 'Error al obtener lista de tarjetas' });
+    res.json(products);
+  });
+};
+
+const getProductDetail = (req, res) => {
+  const id = req.params.id;
+  productModel.findById(id, (err, product) => {
+    if (err || !product) return res.status(404).json({ error: 'Tarjeta no encontrada' });
+    res.json(product);
+  });
+};
+
+const updateProduct = (req, res) => {
+  const id = req.params.id;
+  const balance = req.params.balance;
+  const expirationDate = req.params.expirationDate;
+
+  productModel.update(id,balance,expirationDate, (err, product) => {
+    if (err || !product) return res.status(500).json({ error: 'Error al actualizar la información de la tarjeta' });
+    res.json(product);
+  });
+};
+
+const deleteProduct = (req, res) => {
+  const id = req.params.id;
+  productModel.destroy(id, (err, product) => {
+    if (err || !product) return res.status(500).json({ error: 'Error al eliminar tarjeta' });
+    res.json(product);
+  });
+};
+module.exports = {
+  createProduct,
+  getProducts,
+  getProductDetail,
+  updateProduct,
+  deleteProduct
+};
