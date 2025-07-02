0c91e30482d52698fd7b693c4218037b92b12f2b
diego duchimaza <duchimazadiego68@gmail.com>
2025-07-02 11:41:04 -0500

    primera carga

---
+++ src/configs/db.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const sqlite = require("sqlite3");
+
+const db = new sqlite.Database(
+  path.resolve(__dirname, "database.db"),
+  (error) => {
+    if (error) {
+      return console.error(error);
+    }
+
+    const users = `CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(100) NOT NULL,
+            lastname VARCHAR(100) NOT NULL,
+            email VARCHAR(200) NOT NULL UNIQUE,
+            password VARCHAR(20) NOT NULL
+        )`;
+
+        const giftcards = `CREATE TABLE IF NOT EXISTS giftcards (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title VARCHAR(100) NOT NULL,
+            amount INTEGER,
+            currency VARCHAR(100) NOT NULL,
+            expirationDate VARCHAR(100) NOT NULL,
+            balance REAL
+        )`;
+
+    db.run(users, (error) => {
+      if (error) {
+        return console.error(error);
+      }
+    });
+
+    db.run(giftcards, (error) => {
+      if (error) {
+        return console.error(error);
+      }
+    });
+  }
+);
+
+module.exports = db;
